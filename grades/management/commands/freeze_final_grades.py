9b5661d7f0d6f4e5f4b518e2fe35e9e346b8b586
Giovanni Di Milia <gdimilia@mit.edu>
2016-12-14 17:25:00 -0500

    Added management command to freeze final grades

---
+++ grades/management/commands/freeze_final_grades.py
@@ -0,0 +1,32 @@
+"""
+Freezes final grades for a course
+"""
+from django.core.management import BaseCommand, CommandError
+
+from courses.models import CourseRun
+from grades.tasks import freeze_course_run_final_grades
+
+
+class Command(BaseCommand):
+    """
+    Submits a celery task to freeze the final grades for all the users enrolled in a course run
+    """
+    help = "Submits a celery task to freeze the final grades for all the users enrolled in a course run"
+
+    def add_arguments(self, parser):
+        parser.add_argument("course_id", help="the edx_course_key for the course run")
+
+    def handle(self, *args, **kwargs):  # pylint: disable=unused-argument
+        course_id = kwargs.get('course_id')
+        if course_id is None:
+            raise CommandError('No course_id provided')
+        try:
+            run = CourseRun.objects.get(edx_course_key=course_id)
+        except CourseRun.DoesNotExist:
+            raise CommandError('Course Run for course_is "{}" does not exist'.format(course_id))
+        freeze_course_run_final_grades.delay(run)
+        self.stdout.write(
+            self.style.SUCCESS(
+                'Successfully submitted async task to freeze final grades for course "{}"'.format(course_id)
+            )
+        )
