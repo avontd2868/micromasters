23e3703801ed6b65a7e67d02adc8a9b11bbe3c00
Giovanni Di Milia <gdimilia@mit.edu>
2016-12-20 15:14:46 -0500

    add tests for grades api

--- grades/api_test.py
+++ grades/api_test.py
@@ -1,7 +1,11 @@
 """
 Tests for grades API
 """
+from datetime import datetime, timedelta
 from unittest.mock import patch, MagicMock, ANY
+
+import pytz
+from django.core.exceptions import ImproperlyConfigured
 
 from backends.edxorg import EdxOrgOAuth2
 from courses.factories import CourseRunFactory
@@ -12,6 +16,7 @@ from dashboard.factories import (
     CachedEnrollmentFactory,
 )
 from grades import api
+from grades.models import FinalGrade, FinalGradeStatus
 from micromasters.factories import UserFactory
 from search.base import ESTestCase
 
@@ -33,12 +38,12 @@ class FinalGradeFuncsTests(ESTestCase):
             extra_data={"access_token": "fooooootoken"}
         )
 
-        cls.course_run1 = CourseRunFactory.create()
+        cls.course_run1 = CourseRunFactory.create(freeze_grade_date=datetime.now(tz=pytz.UTC)-timedelta(days=1))
         cls.course_run1.course.program.financial_aid_availability = True
-        cls.course_run2 = CourseRunFactory.create()
+        cls.course_run2 = CourseRunFactory.create(freeze_grade_date=None)
         cls.course_run2.course.program = cls.course_run1.course.program
 
-        cls.course_run3 = CourseRunFactory.create()
+        cls.course_run3 = CourseRunFactory.create(freeze_grade_date=datetime.now(tz=pytz.UTC)+timedelta(days=1))
         cls.course_run3.course.program.financial_aid_availability = False
         cls.course_run4 = CourseRunFactory.create()
         cls.course_run4.course.program = cls.course_run3.course.program
@@ -147,3 +152,84 @@ class FinalGradeFuncsTests(ESTestCase):
         call_arg = non_fa.call_args_list[0][0][0]
         assert isinstance(call_arg, UserCachedRunData)
         assert call_arg.edx_course_key == self.course_run3.edx_course_key
+
+    def test_get_users_final_grade_freeze(self):
+        """
+        tests for get_users_final_grade_freeze function
+        """
+        assert list(api.get_users_final_grade_freeze(self.course_run1)) == [self.user]
+
+        # create another user and enrollment
+        other_user = UserFactory.create()
+        CachedEnrollmentFactory.create(user=other_user, course_run=self.course_run1)
+        assert list(api.get_users_final_grade_freeze(self.course_run1)) == [self.user, other_user]
+
+        # add the user to the FinalGrade model as in progress
+        fg_status = FinalGrade.objects.create(
+            user=other_user, course_run=self.course_run1, status=FinalGradeStatus.PENDING, grade=0.0)
+        assert list(api.get_users_final_grade_freeze(self.course_run1)) == [self.user, other_user]
+
+        # change the final grade status to complete
+        fg_status.status = FinalGradeStatus.COMPLETE
+        fg_status.save()
+        assert list(api.get_users_final_grade_freeze(self.course_run1)) == [self.user]
+
+    @patch('grades.api.get_final_grade', autospec=True)
+    @patch('grades.api._refresh_cache_final_grade', autospec=True)
+    def test_freeze_user_final_grade_error1(self, mock_refr, mock_get_fg):
+        """
+        Test for freeze_user_final_grade function in case of problems with can_freeze_grades
+        """
+        # case not ready to be frozen
+        api.freeze_user_final_grade(self.user, self.course_run3)
+        assert mock_refr.called is False
+        assert mock_get_fg.called is False
+        assert FinalGrade.objects.filter(user=self.user, course_run=self.course_run3).exists() is False
+
+        # case without freeze date
+        with self.assertRaises(ImproperlyConfigured):
+            api.freeze_user_final_grade(self.user, self.course_run2)
+        assert mock_refr.called is False
+        assert mock_get_fg.called is False
+        assert FinalGrade.objects.filter(user=self.user, course_run=self.course_run2).exists() is False
+
+    @patch('grades.api.get_final_grade', autospec=True)
+    @patch('grades.api._refresh_cache_final_grade', autospec=True)
+    def test_freeze_user_final_grade_error2(self, mock_refr, mock_get_fg):
+        """
+        Test for freeze_user_final_grade function in case of problems with refresh of cache
+        """
+        mock_refr.side_effect = AttributeError
+        api.freeze_user_final_grade(self.user, self.course_run1)
+        assert mock_get_fg.called is False
+        mock_refr.assert_called_once_with(self.user)
+        assert FinalGrade.objects.filter(user=self.user, course_run=self.course_run1).exists() is False
+
+    @patch('grades.api.get_final_grade', autospec=True)
+    @patch('grades.api._refresh_cache_final_grade', autospec=True)
+    def test_freeze_user_final_grade_error3(self, mock_refr, mock_get_fg):
+        """
+        Test for freeze_user_final_grade function in case of problems with getting the final grade
+        """
+        mock_get_fg.side_effect = AttributeError
+        api.freeze_user_final_grade(self.user, self.course_run1)
+        mock_refr.assert_called_once_with(self.user)
+        mock_get_fg.assert_called_once_with(self.user, self.course_run1)
+        assert FinalGrade.objects.filter(user=self.user, course_run=self.course_run1).exists() is False
+
+    @patch('grades.api._refresh_cache_final_grade', autospec=True)
+    def test_freeze_user_final_grade(self, mock_refr):
+        """
+        Test for freeze_user_final_grade function in case of problems with getting the final grade
+        """
+        api.freeze_user_final_grade(self.user, self.course_run1)
+        mock_refr.assert_called_once_with(self.user)
+        fg_qset = FinalGrade.objects.filter(user=self.user, course_run=self.course_run1)
+        assert fg_qset.exists() is True
+        fg_status = fg_qset.first()
+        final_grade = api._compute_grade_for_fa(self.user_edx_data.get_run_data(self.course_run1.edx_course_key))
+        assert fg_status.status == FinalGradeStatus.COMPLETE
+        assert fg_status.user == self.user
+        assert fg_status.course_run == self.course_run1
+        assert fg_status.grade == final_grade.grade
+        assert fg_status.passed == final_grade.passed
